28491012f76ab5d7f49029da0a76c87acad5f5fb
psychoSherlock <pschosherlock@gmail.com>
2023-09-20 15:16:59 +0530

    Added Unhash Challange

---
+++ code/unhashme/unhashme.py
@@ -0,0 +1,39 @@
+# Date: 20/09/2023
+# Author: psychoSherlock
+# Website: psychoSherlock.github.io
+# Linkedin: https://www.linkedin.com/in/athul-prakash-nj-564a80226/
+
+import hashlib
+
+
+def hashme(text):
+    md5 = hashlib.md5()
+    md5.update(text.encode('utf-8'))
+    return md5.hexdigest()
+
+def verify_key(key):
+    if len(key) != 7 and (key[2] and key[4] != '.'):
+        return False
+    
+    part1 = key[:4]
+    part2 = key[4:]
+    
+    hash1 = hashme(part1)
+    hash2 = hashme(part2)
+    
+    target_hash1 = "f4bfe0e6c39943f1a630458f43668c31"
+    target_hash2 = "a4e49d067580bcbfa42d6895861d93d6"
+    
+    if hash1 == target_hash1 and hash2 == target_hash2:
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+
+    your_input = "4ck..4ck"
+    
+    if verify_key(your_input):
+        print("Congratulations! You just unhashed me! Follow me for more such challanges: https://github.com/psychoSherlock")
+    else:
+        print("Whoopsie!! Wrong answer!!")
